42aef603fd56c55f1c4d37c9d01c7850266ac648
matjaz <matjaz@192.168.1.205>
2018-12-09 17:21:51 +0100

    webhook page

--- src/si/matjazcerkvenik/dtools/web/WebhookServlet.java
+++ src/si/matjazcerkvenik/dtools/web/WebhookServlet.java
@@ -1,7 +1,11 @@
 package si.matjazcerkvenik.dtools.web;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -14,6 +18,8 @@ import si.matjazcerkvenik.dtools.context.DToolsContext;
 public class WebhookServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 4274913262329715396L;
+	
+	public static List<WebhookMessage> messages = new LinkedList<WebhookMessage>();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -38,19 +44,13 @@ public class WebhookServlet extends HttpServlet {
 		DToolsContext.getInstance().getLogger().info("doGet(): getRemoteUser: " + req.getRemoteUser());
 		DToolsContext.getInstance().getLogger().info("doGet(): getRequestedSessionId: " + req.getRequestedSessionId());
 		DToolsContext.getInstance().getLogger().info("doGet(): getRequestURI: " + req.getRequestURI());
+		DToolsContext.getInstance().getLogger().info("doGet(): getScheme: " + req.getScheme());
+		DToolsContext.getInstance().getLogger().info("doGet(): getServerName: " + req.getServerName());
+		DToolsContext.getInstance().getLogger().info("doGet(): getServerPort: " + req.getServerPort());
+		DToolsContext.getInstance().getLogger().info("doGet(): getServletPath: " + req.getServletPath());
 		
-		DToolsContext.getInstance().getLogger().info("doGet(): getRemotePort: " + req.getScheme());
-		DToolsContext.getInstance().getLogger().info("doGet(): getRemoteUser: " + req.getServerName());
-		DToolsContext.getInstance().getLogger().info("doGet(): getRequestedSessionId: " + req.getServerPort());
-		DToolsContext.getInstance().getLogger().info("doGet(): getRequestURI: " + req.getServletPath());
-		
-		Map<String, String[]> parameterMap = req.getParameterMap();
-		String params = "";
-		for (Iterator<String> it = parameterMap.keySet().iterator(); it.hasNext();) {
-			String s = it.next();
-			params += s + "=" + parameterMap.get(s) + ", ";
-		}
-		DToolsContext.getInstance().getLogger().info("doGet(): parameterMap: " + params);
+		DToolsContext.getInstance().getLogger().info("doGet(): parameterMap: " + getReqParams(req));
+		DToolsContext.getInstance().getLogger().info("doGet(): headers: " + getReqHeaders(req));
 
 	}
 
@@ -58,13 +58,11 @@ public class WebhookServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String name = req.getParameter("name");
-
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getAuthType());
-		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getCharacterEncoding());
-		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getContentLength());
-		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getContentType());
-		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getContextPath());
+		DToolsContext.getInstance().getLogger().info("doPost(): getCharacterEncoding: " + req.getCharacterEncoding());
+		DToolsContext.getInstance().getLogger().info("doPost(): getContentLength: " + req.getContentLength());
+		DToolsContext.getInstance().getLogger().info("doPost(): getContentType: " + req.getContentType());
+		DToolsContext.getInstance().getLogger().info("doPost(): getContextPath: " + req.getContextPath());
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getLocalAddr());
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getLocalName());
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getLocalPort());
@@ -80,6 +78,86 @@ public class WebhookServlet extends HttpServlet {
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getRemoteUser());
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getRequestedSessionId());
 		DToolsContext.getInstance().getLogger().info("doPost(): getAuthType: " + req.getRequestURI());
+		DToolsContext.getInstance().getLogger().info("doPost(): getScheme: " + req.getScheme());
+		DToolsContext.getInstance().getLogger().info("doPost(): getServerName: " + req.getServerName());
+		DToolsContext.getInstance().getLogger().info("doPost(): getServerPort: " + req.getServerPort());
+		DToolsContext.getInstance().getLogger().info("doPost(): getServletPath: " + req.getServletPath());
+		
+		String body = getReqBody(req);
+		DToolsContext.getInstance().getLogger().info("doGet(): parameterMap: " + getReqParams(req));
+		DToolsContext.getInstance().getLogger().info("doPost(): body: " + body);
+		DToolsContext.getInstance().getLogger().info("doPost(): headers: " + getReqHeaders(req));
+		
+		WebhookMessage m = new WebhookMessage();
+		m.setTimestamp(System.currentTimeMillis());
+		m.setContentLength(req.getContentLength());
+		m.setContentType(req.getContentType());
+		m.setMethod(req.getMethod());
+		m.setPathInfo(req.getPathInfo());
+		m.setProtocol(req.getProtocol());
+		m.setRemoteHost(req.getRemoteHost());
+		m.setRemotePort(req.getRemotePort());
+		m.setRequestUri(req.getRequestURI());
+		m.setBody(body);
+		m.setHeaderMap(generateHeaderMap(req));
+		m.setParameterMap(generateParamMap(req));
+		
+		messages.add(m);
+
+	}
+	
+	
+	private Map<String, String> generateHeaderMap(HttpServletRequest req) {
+		
+		Map<String, String> m = new HashMap<String, String>();
+		
+		Enumeration<String> headerNames = req.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String key = headerNames.nextElement();
+			String val = req.getHeader(key);
+			m.put(key, val);
+		}
+		return m;
+	}
+	
+	private Map<String, String> generateParamMap(HttpServletRequest req) {
+		
+		Map<String, String> m = new HashMap<String, String>();
+		Map<String, String[]> parameterMap = req.getParameterMap();
+		
+		for (Iterator<String> it = parameterMap.keySet().iterator(); it.hasNext();) {
+			String s = it.next();
+			m.put(s, parameterMap.get(s)[0]);
+		}
+		return m;
+	}
+	
+	
+	private String getReqHeaders(HttpServletRequest req) {
+		
+		String headers = "";
+		Enumeration<String> headerNames = req.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String key = headerNames.nextElement();
+			//headerNames.toString();
+			String val = req.getHeader(key);
+			headers += key + "=" + val + ", ";
+		}
+		return headers;
+		
+	}
+	
+	private String getReqParams(HttpServletRequest req) {
+		Map<String, String[]> parameterMap = req.getParameterMap();
+		String params = "";
+		for (Iterator<String> it = parameterMap.keySet().iterator(); it.hasNext();) {
+			String s = it.next();
+			params += s + "=" + parameterMap.get(s)[0] + ", ";
+		}
+		return params;
+	}
+	
+	private String getReqBody(HttpServletRequest req) throws IOException {
 		
 		String body = "";
 		String s = req.getReader().readLine();
@@ -88,8 +166,8 @@ public class WebhookServlet extends HttpServlet {
 			s = req.getReader().readLine();
 		}
 		
-		DToolsContext.getInstance().getLogger().info("doPost(): body: " + body);
-
+		return body;
+		
 	}
 
 }
---
+++ src/si/matjazcerkvenik/dtools/web/beans/WebhookBean.java
@@ -0,0 +1,37 @@
+/* 
+ * Copyright (C) 2015 Matjaz Cerkvenik
+ * 
+ * DTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * DTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with DTools. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package si.matjazcerkvenik.dtools.web.beans;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import si.matjazcerkvenik.dtools.web.WebhookMessage;
+import si.matjazcerkvenik.dtools.web.WebhookServlet;
+
+@ManagedBean
+@SessionScoped
+public class WebhookBean {
+	
+	public List<WebhookMessage> getMessages() {
+		return WebhookServlet.messages;
+	}
+	
+}
